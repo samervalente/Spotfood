3745d17481e65a10e046d02304b57b55995f32bb
samer <samervalente@gmail.com>
2022-09-29 11:58:13 -0300

    feat: create initial structure for header component

---
+++ src/assets/shared/Header.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export default function Header(){
+    return (
+        <>
+            <HeaderComponent>
+
+            </HeaderComponent>
+        </>
+    )
+}
+
+const HeaderComponent = styled.div`
+height:80px;
+background-color: lightgray;
+
+`
+
+
